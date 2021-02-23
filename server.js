46001d394aaa798ce4d1755cd398a95a13259644
Artem Senin <artem.senin@nordigy.ru>
2021-02-23 13:47:20 +0300

    feat(server): add `mfe` prefix to routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'bootstrap/dist')));
-app.use('/welcome', express.static(path.join(__dirname, 'welcome/dist')));
-app.use('/music', express.static(path.join(__dirname, 'music/build')));
+app.use('/mfe/welcome', express.static(path.join(__dirname, 'welcome/dist')));
+app.use('/mfe/music', express.static(path.join(__dirname, 'music/build')));
 
 module.exports = app;
