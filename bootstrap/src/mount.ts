9d8fa5b976b77357b7d3ab6f2a790f1d569bbe15
Artem Senin <artem.senin@nordigy.ru>
2021-03-02 00:18:41 +0300

    feat(bootstrap): add unmount function for microfrontend

--- bootstrap/src/mount.ts
+++ bootstrap/src/mount.ts
@@ -1,35 +1,42 @@
-function moveNodeToDocument(parent, document) {
-  return function moveNode(node) {
+const MFE_KEY = 'mfe';
+function adoptNodeToDocument(document: Document): (node: HTMLElement) => HTMLElement {
+  return function cloneNode(node: HTMLElement): HTMLElement {
     // Cloning or Adopting <scripts> nodes doesn't re-evaluate them
     if (node.tagName === 'SCRIPT') {
       const clonedNode = document.createElement(node.tagName);
 
-      [...node.attributes].forEach(attribute => clonedNode.setAttribute(attribute.name, attribute.value));
+      Array.from(node.attributes).forEach(attribute => clonedNode.setAttribute(attribute.name, attribute.value));
       clonedNode.innerHTML = node.innerHTML;
 
-      parent.appendChild(clonedNode);
-      return;
+      return clonedNode;
     }
 
-    const adoptedNode = document.adoptNode(node);
-    parent.appendChild(adoptedNode);
+    return document.adoptNode(node);
   }
 }
 
-function addOrUpdateBaseTag(microFrontendName) {
+function addOrUpdateBaseTag(microFrontendName: string) {
   const baseElement = document.createElement('base');
   baseElement.setAttribute('href', `/mfe/${microFrontendName}/`);
   document.head.appendChild(baseElement);
 }
 
-function mountMicroFrontendInPage(microFrontendName, microFrontendDocument) {
+export function mountMicroFrontendInPage(microFrontendName: string, microFrontendDocument: Document): void {
   addOrUpdateBaseTag(microFrontendName)
 
-  const microFrontendHeadNodes = microFrontendDocument.querySelectorAll('head>*');
-  const microFrontendBodyNodes = microFrontendDocument.querySelectorAll('body>*');
-
-  microFrontendHeadNodes.forEach(moveNodeToDocument(document.head, document))
-  microFrontendBodyNodes.forEach(moveNodeToDocument(document.body, document))
+  const microFrontendHeadNodes = Array.from(microFrontendDocument.querySelectorAll<HTMLElement>('head>*'));
+  const microFrontendBodyNodes = Array.from(microFrontendDocument.querySelectorAll<HTMLElement>('body>*'));
+  const headNodes = microFrontendHeadNodes.map(adoptNodeToDocument(document))
+  const bodyNodes = microFrontendBodyNodes.map(adoptNodeToDocument(document))
+  const clonedNodes = headNodes.concat(bodyNodes);
+  clonedNodes.forEach((node: HTMLElement) => node.dataset[MFE_KEY] = microFrontendName)
+  const appendNode = (parent: Element) => (child: Element) => parent.appendChild(child);
+  headNodes.forEach(appendNode(document.head));
+  bodyNodes.forEach(appendNode(document.body));
 }
 
-export default mountMicroFrontendInPage;
+export function unmountMicroFrontendFromPage(microFrontendName: string): void {
+  const nodeSelector = `[data-${MFE_KEY}="${microFrontendName}"]`;
+  const nodes = document.querySelectorAll<HTMLElement>(nodeSelector);
+  nodes.forEach((node: HTMLElement) => node.remove());
+}
