aa31aeb8039b604791d25dcdd9b3ce55fdd8a14d
Artem Senin <artem.senin@nordigy.ru>
2021-03-10 02:40:35 +0300

    feat(server): change port to 3001

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.all('/*', function(req, res) {
   res.sendFile('index.html', { root: path.join(__dirname, 'bootstrap/dist') });
 });
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3001;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 
