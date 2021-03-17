8226eb7e71ca2ce5775c29b9b325e0ecc02fa294
Artem Senin <artem.senin@nordigy.ru>
2021-03-17 22:40:31 +0300

    feat(welcome): set auth with bootstrap api

--- welcome/src/bootstrap.ts
+++ welcome/src/bootstrap.ts
@@ -1,8 +1,29 @@
 import {IBootstrapPublicAPI} from "../../bootstrap/src/public-api";
 
-export function getBootstrapApi(): IBootstrapPublicAPI {
-  return (window as any).bootstrap;
+function assertBootstrapIsAvailable(bootstrap: any): true | never {
+  if (!bootstrap) {
+    throw new Error('Bootstrap is not available');
+  }
+
+  return true;
 }
+
+function getBootstrapApi(): IBootstrapPublicAPI | never {
+  const bootstrap = (window as any).bootstrap;
+  assertBootstrapIsAvailable(bootstrap);
+  return bootstrap;
+}
+
+let bootstrap: IBootstrapPublicAPI;
+
+function provideBootstrapApi(): IBootstrapPublicAPI | never {
+  if (!bootstrap) {
+    bootstrap = getBootstrapApi();
+  }
+
+  return bootstrap;
+}
+export { provideBootstrapApi as getBootstrapApi }
 
 export function getLifecycleEvents() {
   const bootstrap = getBootstrapApi();
